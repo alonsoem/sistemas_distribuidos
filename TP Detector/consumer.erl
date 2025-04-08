4aaf4bbc8eb50d08115d5bc7af5be79f51faa943
Enrique Alonso <alonso.em@gmail.com>
2025-04-08 10:49:44 -0300

    Manejo de crash

--- TP Detector/consumer.erl
+++ TP Detector/consumer.erl
@@ -2,29 +2,40 @@
     -export([start/1, stop/0]).
 
     start(Producer) ->
-        Consumer = spawn(fun() -> consumer(0) end),
-        register(client, Consumer),
-        Producer! {hello, client}.
+        Consumer = spawn(fun() -> init(Producer,0) end),
+        register(client, Consumer).
+        %Monitor = monitor(process, Producer),
+        %Producer ! {hello, self()},
+        %consumer(0, Monitor).
+
         
 
     stop() ->
         client ! {bye}.
 
+    init(Producer,Ping)->
+        Monitor = monitor(process, Producer),
+        Producer ! {hello, self()},
+        consumer(Ping, Monitor).
 
-    consumer(Ping) ->
+    consumer(Ping,Monitor) ->
         receive
             {ping, N} ->
                 if
                     N==Ping ->
                         io:format("PING  ~w~n", [Ping]),
-                        consumer(N+1);        
+                        consumer(N+1,Monitor);        
                     Ping > N ->
-                        io:format("PING ARRIBA ~w~n", [Ping]),
-                        consumer(N+1)
+                        io:format("PING MAYOR A ~w~n", [Ping]),
+                        consumer(N+1,Monitor)
                 end;
                 
+            {'DOWN', Monitor, process, Object, Info} ->
+                io:format("~w died; ~w~n", [Object, Info]),
+                consumer(Ping, Monitor);
+
             {bye} ->
-                io:format("CONSUMIDOR DETENIDO"),
+                io:format("CONSUMER STOPED"),
                 ok
 
             
