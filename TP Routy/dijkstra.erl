ec99d8fa296f65138294fb3b176ca91fa0d1e90b
Enrique Alonso <alonso.em@gmail.com>
2025-06-03 22:51:44 -0300

    Lista Ordenada

--- TP Routy/dijkstra.erl
+++ TP Routy/dijkstra.erl
@@ -1,12 +1,35 @@
 -module(dijkstra).
--export([update/4]).
+-export([update/4,entry/2,replace/4]).
 
 % update(Node, N, Gateway, Sorted): actualiza la lista Sorted con la
 % información de que Node puede ser alcanzado en N saltos usando Gateway.
 % Si no se encuentra una entrada, no se agrega la nueva entrada. Solo si
 % tenemos un mejor camino (más corto) reemplazamos la entrada existente.
 
+entry(Node, Sorted)->
+   case lists:keyfind(Node, 1, Sorted) of
+    false ->
+        0;
+
+    {NodeTo, Length, NodeFrom} ->
+      Length
+
+   end.
+
+
+
+
+replace(Node, N, Gateway, Sorted)->
+  List = lists:keydelete(Node,1,Sorted),
+  UpdatedList = [{Node,N,Gateway} | List],
+  
+  SortedList = lists:sort(UpdatedList),
+  SortedList.
+  
+
+
 update(Node, N,Gateway, Sorted) ->
+
   % Verifica si ya existe una entrada para el nodo
   case lists:keyfind(Node, 1, Sorted) of
     false ->
