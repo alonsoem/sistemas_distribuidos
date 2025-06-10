a6f017cb63a75ff24b2802587ed6c73447f8eea5
Enrique Alonso <alonso.em@gmail.com>
2025-06-10 00:47:40 -0300

    fix to dijkstra model

--- TP Routy/dijkstra.erl
+++ TP Routy/dijkstra.erl
@@ -14,17 +14,82 @@
   %tomada de la lista ordenada se agrega a la tabla de ruteo.
 
 
+entry(Node, Sorted)->
+   case lists:keyfind(Node, 1, Sorted) of
+    false ->
+        0;
+
+    {NodeTo, Length, NodeFrom} ->
+      Length
+
+   end.
+
+replace(Node, N, Gateway, Sorted)->
+  List = lists:keydelete(Node,1,Sorted),
+  UpdatedList = [{Node,N,Gateway} | List],
+  %agregar funcion lambda para ordenar por N
+  SortedList = lists:keysort(2, UpdatedList),
+  SortedList.
+
+
+update(Node, N,Gateway, Sorted) ->
+
+  % Verifica si ya existe una entrada para el nodo
+  case lists:keyfind(Node, 1, Sorted) of
+    false ->
+      % Si no existe, agrega la nueva entrada
+      Sorted;
+    {Node, OldN, OldGateway} ->
+      % Si existe, verifica si el nuevo camino es mejor
+      if
+        N < OldN ->
+          % Si el nuevo camino es más corto, actualiza la entrada
+          lists:keystore(Node, 1, Sorted, {Node, N, Gateway});
+        true ->
+          % Si no es mejor, deja la lista sin cambios
+          Sorted
+      end
+  end.
+  
+
+
 iterate(Sorted, Map, Table)->
   case Sorted of
       [{Node, inf, Gateway} | Tail] -> Table;
       [{Node, N, Gateway} | Tail]->
-                                ReachableList = lists:map(fun(X)-> {X,Gateway} end , map:reachable(Gateway,Map)),
-                                Reachables = [{Node,Gateway}|ReachableList],
-                                iterate(Tail, Map, lists:append([Reachables,Table]));
+                                ReachableList = map:reachable(Gateway,Map),
+                               %tomo uno y distancia de ese es mayor a 1 lo reemplazamos
+                               NewSorted= mutarSorted(Tail,ReachableList,N,Map,Gateway),
+                                                 
+                                                
+                                  
+                                
+                                iterate(NewSorted, Map, lists:append([[{Node,Gateway}],Table]));
       []->Table
   end.
 
     
+
+mutarSorted(Sorted, Nodes, N, Map, Gateway)->
+ 
+  case Nodes of
+    [Node|Tail]->
+              Length= entry(Node,Sorted),
+    
+              if
+                  Length > N+1 -> 
+                      NewSorted=replace(Node, N+1, Gateway, Sorted),
+                      mutarSorted(NewSorted,Tail,N,Map,Gateway);
+                  true-> 
+                      mutarSorted(Sorted,Tail,N,Map,Gateway)
+              end;
+                  
+    []->Sorted
+  end.
+
+
+
+
 route(Node,Table)->
   case lists:keyfind(Node,1,Table) of
     {To,Gateway} -> {ok,Gateway};
@@ -42,7 +107,8 @@ table(Gateways,Map)->
               end
             end
           ,Nodes),
-          Sorted.
+          iterate(Sorted,Map,[]).
+          
 
 
 oldtable(Gateways, Map) ->
@@ -66,43 +132,5 @@ getLinks(G,L)->
 %route(Node, Table)->
 
 
-entry(Node, Sorted)->
-   case lists:keyfind(Node, 1, Sorted) of
-    false ->
-        0;
-
-    {NodeTo, Length, NodeFrom} ->
-      Length
-
-   end.
 
 
-
-
-replace(Node, N, Gateway, Sorted)->
-  List = lists:keydelete(Node,1,Sorted),
-  UpdatedList = [{Node,N,Gateway} | List],
-  %agregar funcion lambda para ordenar por N
-  SortedList = lists:keysort(2, UpdatedList),
-  SortedList.
-  
-
-
-update(Node, N,Gateway, Sorted) ->
-
-  % Verifica si ya existe una entrada para el nodo
-  case lists:keyfind(Node, 1, Sorted) of
-    false ->
-      % Si no existe, agrega la nueva entrada
-      Sorted;
-    {Node, OldN, OldGateway} ->
-      % Si existe, verifica si el nuevo camino es mejor
-      if
-        N < OldN ->
-          % Si el nuevo camino es más corto, actualiza la entrada
-          lists:keystore(Node, 1, Sorted, {Node, N, Gateway});
-        true ->
-          % Si no es mejor, deja la lista sin cambios
-          Sorted
-      end
-  end.
