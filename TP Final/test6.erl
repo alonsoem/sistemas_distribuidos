95f53e2001396c00b979e367abd2c712678d3171
Nico Alvarez <nicolas.alv3@gmail.com>
2025-06-29 22:15:14 -0300

    feat: implement multiple consumers with ACK/NACK handling in message queue

--- TP Final/test6.erl
+++ TP Final/test6.erl
@@ -1,36 +1,60 @@
-% test6.erl
 -module(test6).
 -export([start/0]).
 
 start() ->
-  E1 = exchange:start(ex1),
-  Q1 = myqueue:start(q1),
-  E1 ! {bind, Q1, rk1},
+  Q = myqueue:start(testq),
 
-  % Consumidor que hace NACK
-  spawn(fun() ->
-    Q1 ! {subscribe, self()},
+  % Consumidor 1: suscribe antes de mensajes
+  C1 = spawn(fun() ->
+    Q ! {subscribe, self()},
     receive acksubsc -> ok end,
     receive
-      {msg, Msg} ->
-        io:format("Consumidor 1 recibe: ~p~n", [Msg]),
-        timer:sleep(500),
-        Q1 ! {nack, Msg}
+      {msg, M1} ->
+        io:format("C1 recibe: ~p~n", [M1]),
+        Q ! {ack, M1}
     end
-        end),
+             end),
 
-  % Consumidor que hace ACK
-  spawn(fun() ->
-    Q1 ! {subscribe, self()},
+  timer:sleep(100),
+
+  % Enviar mensaje sin consumidores disponibles
+  Q2 = myqueue:start(testq2),
+  Q2 ! {msg, "msg_sin_consumidor", none},
+  timer:sleep(100),
+  C2 = spawn(fun() ->
+    Q2 ! {subscribe, self()},
     receive acksubsc -> ok end,
     receive
-      {msg, Msg} ->
-        io:format("Consumidor 2 recibe: ~p~n", [Msg]),
-        timer:sleep(500),
-        Q1 ! {ack, Msg}
+      {msg, M2} ->
+        io:format("C2 recibe: ~p~n", [M2]),
+        Q2 ! {ack, M2}
     end
-        end),
+             end),
 
-  % Publicar mensaje
-  E1 ! {msg, "Mensaje para ACK/NACK", rk1},
-  timer:sleep(2000).
+  timer:sleep(100),
+
+  % Consumidor 3: NACK
+  C3 = spawn(fun() ->
+    Q ! {subscribe, self()},
+    receive acksubsc -> ok end,
+    receive
+      {msg, M3} ->
+        io:format("C3 recibe: ~p~n", [M3]),
+        Q ! {nack, M3},
+        receive
+          {msg, M4} ->
+            io:format("C3 recibe de nuevo: ~p~n", [M4]),
+            Q ! {ack, M4}
+        end
+    end
+             end),
+
+  timer:sleep(100),
+
+  % Enviar mensajes a la cola con consumidores
+  Q ! {msg, "msg1", none},
+  Q ! {msg, "msg2", none},
+
+  timer:sleep(1000),
+
+  ok.
